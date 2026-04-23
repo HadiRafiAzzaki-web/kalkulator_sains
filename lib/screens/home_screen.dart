7a039c5f806e7d2fb02e71c218e0665c6fe75beb
Hadi Rafi <hermesmerlin285@gmail.com>
2026-04-24 06:48:56 +0700

    menambah mode volt dan ampere

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final voltC = TextEditingController();
+  final ampC = TextEditingController();
   final jumlahC = TextEditingController();
   final wattC = TextEditingController();
   final waktuC = TextEditingController();
@@ -18,12 +20,19 @@ class _HomeScreenState extends State<HomeScreen> {
 
   double hasil = 0;
   double kwh = 0;
+  bool isWatt = true;
   List<Logic> history = [];
 
   @override
   void initState() {
     loadData();
     super.initState();
+  }
+
+  void isWattOrVoltAmpere() {
+    setState(() {
+      isWatt = !isWatt;
+    });
   }
 
   void loadData() async {
@@ -33,11 +42,54 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void hitungWatt() async {
+    double jumlah = double.tryParse(jumlahC.text) ?? 0;
+    double waktu = double.tryParse(waktuC.text) ?? 0;
+    double tarif = double.tryParse(tarifC.text) ?? 0;
+    double volt = double.tryParse(voltC.text) ?? 0;
+    double ampere = double.tryParse(ampC.text) ?? 0;
+
+    double hasilWatt = volt * ampere;
+
+    if (voltC.text.isEmpty ||
+        ampC.text.isEmpty ||
+        waktuC.text.isEmpty ||
+        tarifC.text.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Isi semua data dulu")));
+      return;
+    }
+
+    final data = Service.buatData(
+      jumlah: jumlah,
+      watt: hasilWatt,
+      waktu: waktu,
+      tarif: tarif,
+    );
+
+    await DbService.insert(data);
+
+    setState(() {
+      hasil = data.hasil;
+      kwh = data.kwh;
+    });
+
+    loadData();
+  }
+
   void hitung() async {
     double jumlah = double.tryParse(jumlahC.text) ?? 0;
     double watt = double.tryParse(wattC.text) ?? 0;
     double waktu = double.tryParse(waktuC.text) ?? 0;
     double tarif = double.tryParse(tarifC.text) ?? 0;
+
+    if (wattC.text.isEmpty || waktuC.text.isEmpty || tarifC.text.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Isi semua data dulu")));
+      return;
+    }
 
     final data = Service.buatData(
       jumlah: jumlah,
@@ -48,7 +100,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
     await DbService.insert(data);
 
-    setState(() => hasil = data.hasil);
+    setState(() {
+      hasil = data.hasil;
+      kwh = data.kwh;
+    });
 
     loadData();
   }
@@ -59,12 +114,12 @@ class _HomeScreenState extends State<HomeScreen> {
       child: TextField(
         controller: c,
         keyboardType: TextInputType.number,
-        style: TextStyle(color: Colors.white),
+        style: const TextStyle(color: Colors.white),
         decoration: InputDecoration(
           labelText: label,
-          labelStyle: TextStyle(color: Colors.grey),
+          labelStyle: const TextStyle(color: Colors.grey),
           filled: true,
-          fillColor: Color(0xFF0f3460),
+          fillColor: const Color(0xFF0f3460),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
             borderSide: BorderSide.none,
@@ -95,16 +150,31 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               child: Column(
                 children: [
-                  input(jumlahC, "Jumlah"),
-                  input(wattC, "Watt"),
-                  input(waktuC, "Jam"),
-                  input(tarifC, "Tarif"),
+                  Text(
+                    isWatt ? "Mode: Watt" : "Mode: Volt & Ampere",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  ElevatedButton(
+                    onPressed: isWattOrVoltAmpere,
+                    child: Text(
+                      isWatt ? 'Ganti ke Volt/Ampere' : 'Ganti ke Watt',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  input(jumlahC, "Jumlah Perangkat"),
+                  if (isWatt) input(wattC, "Watt"),
+                  if (!isWatt) input(voltC, "Volt"),
+                  if (!isWatt) input(ampC, "Ampere"),
+                  input(waktuC, "Waktu penggunaan(per jam)"),
+                  input(tarifC, "Tarif listrik"),
                   SizedBox(height: 10),
                   ElevatedButton(
                     onPressed: () {
-                      setState(() {
+                      if (isWatt) {
                         hitung();
-                      });
+                      } else {
+                        hitungWatt();
+                      }
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFF4e54c8),
